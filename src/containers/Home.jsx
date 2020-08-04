916861022adf9f88f63ea29a69783024209086fd
min <pakmk8@gmail.com>
2020-08-03 21:54:25 -0400

    added search function

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 class Home extends Component {
   state = {
+    search: "",
     sortAsc: true,
     employees: [
       {
@@ -57,21 +58,59 @@ class Home extends Component {
       // console.log(sortedEmployees);
       this.setState({
         employees: sortedEmployees,
-        sortAsc: false
+        sortAsc: false,
       });
     } else {
       const sortedEmployees = this.state.employees.sort(compareDesc);
       // console.log(sortedEmployees);
       this.setState({
         employees: sortedEmployees,
-        sortAsc: true
+        sortAsc: true,
       });
     }
   };
 
+  handleSubmit = (event) =>{
+    event.preventDefault();
+    this.filterEmployees();
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  filterEmployees = () => {
+    const searchTerm = this.state.search.toLowerCase();
+    this.setState ({
+      employees: this.state.employees.filter((employee)=>
+    employee.name.toLowerCase().includes(searchTerm)
+      ),
+    });
+  }
+
   render() {
     return (
       <div className="container">
+        <div className="col-sm-8">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search"
+                name="search"
+                value={this.state.search}
+                onChange={this.handleInputChange}
+              />
+              <button type="submit" className="btn btn-primary">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
         <div className="row">
           <div className="col">
             <table className="table table-striped">
